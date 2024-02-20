15870c57e877173377626f89595f939515209023
Phuong Mai Nguyen <phuong.nguyen@aotech.net.au>
2024-02-20 13:09:44 +1100

    feat: Implement error handling

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
+const compoundedTermObj = {
+  Monthly: 12,
+  Quarterly: 4,
+  Annually: 1,
+  AtMaturity: 0,
+};
+
 export function calculateTermDeposit(
   deposit,
   interestRatePercent,
   termInMonths,
   interestPaidFrequency
 ) {
+  deposit = Number(deposit);
+  interestRatePercent = Number(interestRatePercent);
+  termInMonths = Number(termInMonths);
+
+  if (Number.isNaN(deposit)) return "Deposit amount is not a valid number";
+  if (Number.isNaN(interestRatePercent))
+    return "Interest rate is not a valid number";
+  if (Number.isNaN(termInMonths))
+    return "Investment term (months) is not a valid number";
+
+  if (compoundedTermObj[interestPaidFrequency] === undefined)
+    return "Interest paid frequency is not a valid option";
+
   const interestRateDecimal = interestRatePercent / 100;
   const termInYears = termInMonths / 12;
-  const compoundedTerm =
-    interestPaidFrequency === "Monthly"
-      ? 12
-      : interestPaidFrequency === "Quarterly"
-      ? 4
-      : interestPaidFrequency === "Annually"
-      ? 1
-      : 0;
+  const compoundedTerm = compoundedTermObj[interestPaidFrequency];
 
   let balance;
-
   if (compoundedTerm > 0)
     balance =
       deposit *
--- script.test.js
+++ script.test.js
@@ -2,19 +2,19 @@ import { calculateTermDeposit } from "./script";
 
 describe("calculateTermDeposit()", () => {
   it.each`
-    deposit    | interestRatePercent | investmentTermInMonths | interestPaidFrequency | expectedBalance
-    ${1000}    | ${0}                | ${3}                   | ${"Monthly"}          | ${1000}
-    ${10000}   | ${1.1}              | ${36}                  | ${"Monthly"}          | ${10335}
-    ${10000}   | ${1.1}              | ${36}                  | ${"Quarterly"}        | ${10335}
-    ${10000}   | ${1.1}              | ${36}                  | ${"Annually"}         | ${10334}
-    ${10000}   | ${1.1}              | ${36}                  | ${"AtMaturity"}       | ${10330}
-    ${1500000} | ${0.1}              | ${60}                  | ${"AtMaturity"}       | ${1507500}
+    deposit    | interestRatePercent | termInMonths | interestPaidFrequency | expectedBalance
+    ${1000}    | ${0}                | ${3}         | ${"Monthly"}          | ${1000}
+    ${"10000"} | ${1.1}              | ${36}        | ${"Monthly"}          | ${10335}
+    ${10000}   | ${1.1}              | ${36}        | ${"Quarterly"}        | ${10335}
+    ${10000}   | ${"1.1"}            | ${36}        | ${"Annually"}         | ${10334}
+    ${10000}   | ${1.1}              | ${"36"}      | ${"AtMaturity"}       | ${10330}
+    ${1500000} | ${0.1}              | ${60}        | ${"AtMaturity"}       | ${1507500}
   `(
-    "should return the balance $expectedBalance given deposit $deposit interestRatePercent $interestRatePercent investmentTermInMonths $investmentTermInMonths interestPaidFrequency $interestPaidFrequency",
+    "should return the balance $expectedBalance given deposit $deposit interestRatePercent $interestRatePercent termInMonths $termInMonths interestPaidFrequency $interestPaidFrequency",
     ({
       deposit,
       interestRatePercent,
-      investmentTermInMonths,
+      termInMonths,
       interestPaidFrequency,
       expectedBalance,
     }) => {
@@ -22,10 +22,74 @@ describe("calculateTermDeposit()", () => {
         calculateTermDeposit(
           deposit,
           interestRatePercent,
-          investmentTermInMonths,
+          termInMonths,
           interestPaidFrequency
         )
       ).toBe(expectedBalance);
     }
   );
+
+  it("should return an error when deposit is not a number", () => {
+    const deposit = "012b";
+    const interestRatePercent = 3.4;
+    const termInMonths = 2;
+    const interestPaidFrequency = "Monthly";
+
+    expect(
+      calculateTermDeposit(
+        deposit,
+        interestRatePercent,
+        termInMonths,
+        interestPaidFrequency
+      )
+    ).toBe("Deposit amount is not a valid number");
+  });
+
+  it("should return an error when interest rate is not a number", () => {
+    const deposit = 15000;
+    const interestRatePercent = "3.4  /n";
+    const termInMonths = 2;
+    const interestPaidFrequency = "Monthly";
+
+    expect(
+      calculateTermDeposit(
+        deposit,
+        interestRatePercent,
+        termInMonths,
+        interestPaidFrequency
+      )
+    ).toBe("Interest rate is not a valid number");
+  });
+
+  it("should return an error when investment term (months) is not a number", () => {
+    const deposit = 15000;
+    const interestRatePercent = 3.4;
+    const termInMonths = "20 months";
+    const interestPaidFrequency = "Monthly";
+
+    expect(
+      calculateTermDeposit(
+        deposit,
+        interestRatePercent,
+        termInMonths,
+        interestPaidFrequency
+      )
+    ).toBe("Investment term (months) is not a valid number");
+  });
+
+  it("should return an error when interest paid frequency is not a valid option", () => {
+    const deposit = 15000;
+    const interestRatePercent = 3.4;
+    const termInMonths = 20;
+    const interestPaidFrequency = "Fortnightly";
+
+    expect(
+      calculateTermDeposit(
+        deposit,
+        interestRatePercent,
+        termInMonths,
+        interestPaidFrequency
+      )
+    ).toBe("Interest paid frequency is not a valid option");
+  });
 });
